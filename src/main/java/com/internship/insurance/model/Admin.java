9c4bbd4f6911c18544330eda49b649c0f00a318e
Denis Dobos <ddobos@mtc.md>
2019-05-07 15:51:33 +0300

    add model Admin

--- src/main/java/com/internship/insurance/model/Admin.java
+++ src/main/java/com/internship/insurance/model/Admin.java
@@ -1,4 +1,52 @@
 package com.internship.insurance.model;
 
+@Entity
+@Table(name="employees")
 public class Admin {
+    @Id
+    @GenerationType(Strategy=GenetationType.IDENTITY)
+    private Long id;
+    private String login;
+    private Sting password;
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Sting getPassword() {
+        return password;
+    }
+
+    public void setPassword(Sting password) {
+        this.password = password;
+    }
+
+    public com.internship.insurance.model.Role getRole() {
+        return role;
+    }
+
+    public void setRole(com.internship.insurance.model.Role role) {
+        this.role = role;
+    }
 }
+
+enum Role{
+    ADMIN,
+    MODERATOR,
+    USER
+}
+
+
