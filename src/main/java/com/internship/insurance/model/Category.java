129c9be0b5458cef31b54dde163eb7a718f59ae4
was30 <gurduzavasile@gmail.com>
2019-05-08 09:09:14 +0300

    added category and property models

--- src/main/java/com/internship/insurance/model/Category.java
+++ src/main/java/com/internship/insurance/model/Category.java
@@ -1,4 +1,36 @@
 package com.internship.insurance.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    public Category() {
+
+    }
+
+    public Category(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- src/main/java/com/internship/insurance/model/Property.java
+++ src/main/java/com/internship/insurance/model/Property.java
@@ -1,4 +1,55 @@
 package com.internship.insurance.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "properties")
 public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Category category;
+    private double coefficient;
+
+    public Property() {
+    }
+
+    public Property(String title, Category category, double coefficient) {
+        this.title = title;
+        this.category = category;
+        this.coefficient = coefficient;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(double coefficient) {
+        this.coefficient = coefficient;
+    }
 }
