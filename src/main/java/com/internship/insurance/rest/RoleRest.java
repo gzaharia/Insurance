dda19c3f6ac443a0561d60713c7b69b557a27fd8
silver <Mihx222@gmail.com>
2019-05-15 12:16:57 +0300

    Updated user access.

---
+++ src/main/java/com/internship/insurance/rest/RoleRest.java
@@ -0,0 +1,26 @@
+package com.internship.insurance.rest;
+
+import com.internship.insurance.model.Role;
+import com.internship.insurance.repository.RoleRepo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/roles")
+public class RoleRest {
+
+    private final RoleRepo roleRepo;
+
+    public RoleRest(RoleRepo roleRepo) {
+        this.roleRepo = roleRepo;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return ResponseEntity.ok(roleRepo.findAll());
+    }
+}
